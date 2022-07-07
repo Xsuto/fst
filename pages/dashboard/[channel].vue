83507fe1601e046b52713267eb40c49c8d5230a9
Xsuto <michal.sutowicz@gmail.com>
2022-07-07 19:08:16 +0200

    remove dead code

--- pages/dashboard/[channel].vue
+++ pages/dashboard/[channel].vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
 const route = useRoute()
-const dashboard = ref(null)
 const href = computed(() => window.location.host.split(':')[0])
 const channel = computed(() => route.params.channel)
 const link = computed(() => `https://player.twitch.tv/?channel=${channel.value}&parent=${href.value}`)
 </script>
 
 <template>
-  <div ref="dashboard" class="dashboard">
+  <div class="dashboard">
     <twitch-iframe :link="link" />
     <Chat :channel="channel" />
   </div>
