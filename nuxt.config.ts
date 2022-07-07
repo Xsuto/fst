c48287e96ad5b483f14e3f2a57b19cecd0c630c5
Xsuto <michal.sutowicz@gmail.com>
2022-07-07 19:03:05 +0200

    add vueuse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  buildModules: [
+  modules: [
+    '@vueuse/nuxt',
     ['unplugin-icons/nuxt', { /* options */ }],
   ],
   ssr: false,
