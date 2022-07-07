7e05d2ecc82ae4ab7338fe692e1ea850400eb02d
Xsuto <michal.sutowicz@gmail.com>
2022-07-07 03:54:25 +0200

    eslint max-len

--- components/Message.vue
+++ components/Message.vue
@@ -10,10 +10,16 @@ const messageContent = $computed(() => props.message.text.split(' '))
 
 <template>
   <li :style="{ color: props.message.tags.color }">
-    <span v-if="props.message.tags.mod"><img class="badge moderator" src="https://static-cdn.jtvnw.net/badges/v1/3267646d-33f0-4b17-b3df-f923a41db1d0/2"></span>
-    <span v-if="props.message.tags.subscriber"><img class="badge" src="https://static-cdn.jtvnw.net/badges/v1/0ba17a24-d560-45d1-9b8d-6c133bb637f9/2"></span>
+    <span v-if="props.message.tags.mod">
+      <img class="badge moderator" src="https://static-cdn.jtvnw.net/badges/v1/3267646d-33f0-4b17-b3df-f923a41db1d0/2">
+    </span>
+    <span v-if="props.message.tags.subscriber">
+      <img class="badge" src="https://static-cdn.jtvnw.net/badges/v1/0ba17a24-d560-45d1-9b8d-6c133bb637f9/2">
+    </span>
     <p>{{ props.message.tags["display-name"] }}:</p>
-    <span v-for="(word, i) in messageContent" :key="i"><img v-if="word.startsWith('https')" :src="word"><span v-else>{{ word }}</span></span>
+    <span v-for="(word, i) in messageContent" :key="i">
+      <img v-if="word.startsWith('https')" :src="word"><span v-else>{{ word }}</span>
+    </span>
   </li>
 </template>
 
